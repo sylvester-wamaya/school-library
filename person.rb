94d9e23bac25474978fad8de6c4cd1dd8fd0c842
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-08-02 00:30:22 +0300

    Add public methods

--- person.rb
+++ person.rb
@@ -1,14 +1,22 @@
 class Person
+
+    attr_accessor :name, :age
+    attr_writer :id
+
     def initialize(name = "Unknown", age, parent_permission = true)
         @id
         @name = name
         @age = age
+        @parent_permission = parent_permission
     end
     
-    attr_accessor :name, :age
-    attr_writer :id
 
-    Private
+    def can_use_services?
+        of_age? || @parent_permission
+
+    end
+
+    private
 
     def of_age?
         @age >= 18
