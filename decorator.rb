54dabc042e12cf0e5874f9b3e02a522bbebfae1f
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-08-03 19:50:43 +0300

    Implement decorator

---
+++ decorator.rb
@@ -0,0 +1,11 @@
+require './nameable'
+# A Decorator class
+class Decorator < Nameable
+    def initialize(nameable)
+        @nameable = nameable
+    end
+
+    def correct_name 
+        @nameable.correct_name
+    end
+end
