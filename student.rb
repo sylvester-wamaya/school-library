8e45f1f4737e30b6ff00b3265fae0cb365389d4b
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-08-04 21:02:52 +0300

    Create association between classrom and student

--- student.rb
+++ student.rb
@@ -5,6 +5,8 @@ class Student < Person
   def initialize(id, age, classroom, parent_permission, name)
     super(id, name, age, parent_permission)
     @classroom = classroom
+    classroom.students.push(self) unless classroom.include?(self)      
+    end
   end
 
   def play_hooky
